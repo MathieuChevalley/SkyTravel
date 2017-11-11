c5f76cffc98db68ed215484787f767b739548300
Mathieu <mathieu.chevalley@epfl.ch>
2017-11-11 19:49:30 +0100

    settings menu

--- app/src/main/java/com/lauzhack/skytravel/MainActivity.java
+++ app/src/main/java/com/lauzhack/skytravel/MainActivity.java
@@ -85,6 +85,10 @@ public class MainActivity extends AppCompatActivity {
                     });
 
                 }
+                else {
+                    resultArray.clear();
+                    mAdapter.notifyDataSetChanged();
+                }
             }
         });
 
